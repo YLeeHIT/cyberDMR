b27713aa57d1bc121b92640c840f697dd9e4fe79
Yang Li <yli21b@hit.edu.cn>
2025-09-15 16:41:57 +0800

    Update run_demo.sh

--- demo/run_demo.sh
+++ demo/run_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash ../cyberDMR.sh -i ./demo/input -o ./demo/output -chr chr21 -g1 lethal -g2 normal -d 0.1 -q 0.05
-bash simulate_data.sh -o demo/simulate_data -t 100
+bash ../cyberDMR.sh -i ./input -o ./output -chr chr21 -g1 lethal -g2 normal -d 0.1 -q 0.05
+bash ../simulate_data.sh -o ./simulate_data -t 100
